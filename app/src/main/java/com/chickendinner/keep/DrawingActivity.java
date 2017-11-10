b4d63934f38c88f97c08cfe93685afffaba748e0
CuanT1anHou <jwang.henry@gmail.com>
2017-11-09 16:13:36 -0800

    add modify to github

--- app/src/main/java/com/chickendinner/keep/DrawingActivity.java
+++ app/src/main/java/com/chickendinner/keep/DrawingActivity.java
@@ -1,29 +1,166 @@
 package com.chickendinner.keep;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Environment;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.chickendinner.keep.workbox.PathView;
 
-public class DrawingActivity extends AppCompatActivity {
+public class DrawingActivity extends NoteActivity {
+    private ImageButton backButton, undoButton, redoButton, deleteButton, cropButton, drawButton, eraseButton;
+    private ImageView iv_canvas;
+    private Bitmap baseBitmap;
+    private Canvas canvas;
+    private Paint paint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawing);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // 初始化一个画笔，笔触宽度为5，颜色为红色
+        paint = new Paint();
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.RED);
+
+        iv_canvas = (ImageView) findViewById(R.id.iv_canvas);
+        backButton = (ImageButton) findViewById(R.id.backButton);
+        undoButton = (ImageButton) findViewById(R.id.undoButton);
+        redoButton = (ImageButton) findViewById(R.id.redoButton);
+        deleteButton = (ImageButton) findViewById(R.id.deleteButton);
+        cropButton = (ImageButton) findViewById(R.id.cropButton);
+        drawButton = (ImageButton) findViewById(R.id.drawButton);
+        eraseButton = (ImageButton) findViewById(R.id.eraseButton);
+
+        backButton.setOnClickListener(click);
+        undoButton.setOnClickListener(click);
+        redoButton.setOnClickListener(click);
+        deleteButton.setOnClickListener(click);
+        cropButton.setOnClickListener(click);
+        drawButton.setOnClickListener(click);
+        eraseButton.setOnClickListener(click);
+        iv_canvas.setOnTouchListener(touch);
     }
 
+    private View.OnTouchListener touch = new OnTouchListener() {
+        // 定义手指开始触摸的坐标
+        float startX;
+        float startY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                // 用户按下动作
+                case MotionEvent.ACTION_DOWN:
+                    // 第一次绘图初始化内存图片，指定背景为白色
+                    if (baseBitmap == null) {
+                        baseBitmap = Bitmap.createBitmap(iv_canvas.getWidth(),
+                                iv_canvas.getHeight(), Bitmap.Config.ARGB_8888);
+                        canvas = new Canvas(baseBitmap);
+                        canvas.drawColor(Color.WHITE);
+                    }
+                    // 记录开始触摸的点的坐标
+                    startX = event.getX();
+                    startY = event.getY();
+                    break;
+                // 用户手指在屏幕上移动的动作
+                case MotionEvent.ACTION_MOVE:
+                    // 记录移动位置的点的坐标
+                    float stopX = event.getX();
+                    float stopY = event.getY();
+
+                    //根据两点坐标，绘制连线
+                    canvas.drawLine(startX, startY, stopX, stopY, paint);
+
+                    // 更新开始点的位置
+                    startX = event.getX();
+                    startY = event.getY();
+
+                    // 把图片展示到ImageView中
+                    iv_canvas.setImageBitmap(baseBitmap);
+                    break;
+                case MotionEvent.ACTION_UP:
+
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    };
+    private View.OnClickListener click = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.backButton:
+                    finish();
+                    break;
+                case R.id.undoButton:
+                    break;
+                case R.id.redoButton:
+                    break;
+                case R.id.deleteButton:
+                    resumeCanvas();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * 保存图片到SD卡上
+     */
+//    protected void saveBitmap() {
+//        try {
+//            // 保存图片到SD卡上
+//            File file = new File(Environment.getExternalStorageDirectory(),
+//                    System.currentTimeMillis() + ".png");
+//            FileOutputStream stream = new FileOutputStream(file);
+//            baseBitmap.compress(CompressFormat.PNG, 100, stream);
+//            Toast.makeText(DrawingActivity.this, "保存图片成功", 0).show();
+//
+//            // Android设备Gallery应用只会在启动的时候扫描系统文件夹
+//            // 这里模拟一个媒体装载的广播，用于使保存的图片可以在Gallery中查看
+//            Intent intent = new Intent();
+//            intent.setAction(Intent.ACTION_MEDIA_MOUNTED);
+//            intent.setData(Uri.fromFile(Environment
+//                    .getExternalStorageDirectory()));
+//            sendBroadcast(intent);
+//        } catch (Exception e) {
+//            Toast.makeText(DrawingActivity.this, "保存图片失败", 0).show();
+//            e.printStackTrace();
+//        }
+//    }
+
+    /**
+     * 清除画板
+     */
+    protected void resumeCanvas() {
+        // 手动清除画板的绘图，重新创建一个画板
+        if (baseBitmap != null) {
+            baseBitmap = Bitmap.createBitmap(iv_canvas.getWidth(), iv_canvas.getHeight(), Bitmap.Config.ARGB_8888);
+            canvas = new Canvas(baseBitmap);
+            canvas.drawColor(Color.WHITE);
+            iv_canvas.setImageBitmap(baseBitmap);
+            //Toast.makeText(DrawingActivity.this, "清除画板成功，可以重新开始绘图", 0).show();
+        }
+    }
 }
